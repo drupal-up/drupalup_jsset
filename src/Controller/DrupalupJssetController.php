48561b60973f4668da6b05e649dad1ae55e7b04f
flesheater <nikolay.r.borisov@gmail.com>
2018-10-01 21:22:19 +0200

    Js updates to get the site.title.

--- src/Controller/DrupalupJssetController.php
+++ src/Controller/DrupalupJssetController.php
@@ -2,18 +2,21 @@
 
 namespace Drupal\drupalup_jsset\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
+
 /**
  * Output of our JS page.
  */
-class DrupalupJssetController {
+class DrupalupJssetController extends ControllerBase {
 
   public function jsPage() {
+
     $build = [];
     $build['content'] = [
       '#markup' => '<div class="js-var">Our JS Page</div>',
     ];
     $build['#attached']['library'][] = 'drupalup_jsset/js_example';
-    $build['#attached']['drupalSettings']['js_example']['title'] = 'Test text';
+    $build['#attached']['drupalSettings']['js_example']['title'] = $this->config('system.site')->get('name');
 
     return $build;
   }
